e8834e7286a6a16ec030ef9b9ea00bbca191ca38
Hammadomar1 <105552469+Hammadomar1@users.noreply.github.com>
2023-09-19 20:24:01 +0300

    Create uart_rx_startbit_checker.v

---
+++ uart_rx_startbit_checker.v
@@ -0,0 +1,22 @@
+module start_bit_checker (
+    input UCLK,
+    input reset,
+    input enable,
+    input sampled_bit,
+    
+    output reg start_bit_error 
+);
+
+  always @(posedge UCLK or negedge reset) begin
+        if (~reset) begin
+            start_bit_error <= 1'b0;
+        end
+        else if (enable) begin
+            start_bit_error <= sampled_bit;
+        end
+        else begin
+            start_bit_error <= 1'b0;
+        end
+    end
+
+endmodule
