89ddb5449b694ad6182da963e85aab196be1052c
Hammadomar1 <105552469+Hammadomar1@users.noreply.github.com>
2023-09-20 13:51:17 +0300

    Update uart_tx_parity.v

--- uart_tx_parity.v
+++ uart_tx_parity.v
@@ -1,12 +1,14 @@
 module uart_tx_parity # (parameter DATA_WIDTH = 8)
 (
-    input UCLK,
-    input reset,
+    input wire UCLK,
+    input wire reset,
     input parity_en,
     input data_valid,
     input [DATA_WIDTH - 1:0] parallel_data,
+    input wire err_inj_en,
     
-    output reg parity_bit
+    output reg parity_bit,
+    output wire error_inj_done,
 );
 
 
@@ -18,12 +20,15 @@ module uart_tx_parity # (parameter DATA_WIDTH = 8)
           begin
               if (err_inj_en)
                       begin 
-                            parity_bit <= ^parallel_data;
+                        parity_bit <= ^parallel_data;
+                        error_inj_done <=   1'b1;
                       end
+              
               else
                 // Odd parity
                             parity_bit <= ~^parallel_data;
           end
+        
         end
     end
 
